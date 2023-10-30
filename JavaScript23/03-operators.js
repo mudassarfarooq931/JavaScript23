d45a0a959fc93482263906787291022f7328f834
Mudassar Malik <94029576+mudassarfarooq931@users.noreply.github.com>
2023-10-30 18:22:57 +0000

    operators

---
+++ JavaScript23/03-operators.js
@@ -0,0 +1,22 @@
+// Opeerators in JS
+
+// 1 Arithmetic Operators ( + , - , * , / , % , **)
+
+let num1 = 25;
+let num2 = 4;
+
+let sum = num1 + num2 ; 
+let sub = num1 - num2 ;
+let mul = num1 * num2 ;
+let div = num1 / num2 ;
+let mod = num1 % num2 ;
+let expo = num1 ** num2 ;
+
+console.log("Sum is : ", sum);
+console.log("Subtraction  is : ", sub);
+console.log("Multiplication is : ", mul);
+console.log("Division is : ", div);
+console.log("Modulus is : ", mod);
+console.log("Exponentiation is : ", expo);
+
+// Opeerators in JS
